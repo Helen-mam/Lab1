788ee4e4d30fd5d7708fe2d76a708d5851ce7001
Viktory <blacklastday@gmail.com@git config --global user.email blacklastday@gmail.comgit config --global user.email blacklastday@gmail.com@>
2021-01-23 20:11:09 +0300

    Первая лаба

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE htm1>
+<html>
+	<head>
+		<title> Моя страничка </title>
+		<link rel="stylesheet" href="mycss.css">
+		<link rel="shortcut icon" href="favicon.ico">
+	</head>
+	<body>
+		<div class="mydiv1">
+			<?php
+				echo "<h1>Радиус круга 3, площадь этого круга равняется  ", M_PI * 9;"</h1>"
+			?>
+		</div>
+	</body>
+</html>
